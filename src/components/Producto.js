c48d7a5ba7042adc92edcfa1af06ee54451faa6f
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-09 16:30:44 -0600

    Se aregaron stylos inline a los productos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,12 +1,23 @@
 import { Component } from "react"
 
+const styles ={ //estilos para el  div del producto
+    producto: {
+        border: 'solid 2px #eee',
+        boxShadow: '0px 5px 5px #154360',
+        width: '30%',
+    },
+    img:{
+        width: '100%'
+    }
+}
+
 class Producto extends Component{
     render() {
         console.log(this.props) //ver la info del producto
         const {producto } = this.props
         return (
-            <div>
-                <img alt={producto} src={producto.img} height={200}></img>
+            <div style={styles.producto}>
+                <img style={styles.img} alt={producto} src={producto.img} ></img>
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
                 
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,12 +1,20 @@
 import { Component  } from "react";
 import Producto from "./Producto"
 
+const styles = { //estilos para el div que contiene los productos
+    productos: {
+        display : 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+}
+
 class Productos extends Component {
     render() {
         const { productos, agregarAlCarro } = this.props
 
         return ( //siempre retorna algo el componente
-            <div>
+            <div style={styles.productos}>
                 {productos.map(producto => <Producto
                 agregarAlCarro = {agregarAlCarro}
                 key = {producto.name}

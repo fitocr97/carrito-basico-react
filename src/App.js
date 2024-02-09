c683d8c8b7c6b2cc7d7f8217173dd9988a166284
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-09 09:48:27 -0600

    se agregaron imagenes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Component } from "react";  
 
 class App extends Component{
+  state = {
+    productos: [
+      { name: 'colibri', price: 2000, img: '/productos/colibri.jpg'},
+      { name: 'lapa', price: 1500, img: '/productos/lapa.jpg'},
+      { name: 'shell', price: 3000, img: '/productos/shell.jpg'}
+    ]
+  }
+
   render () {
     return (
       <p>hola mundo</p>
